d7312dc4a2ceb4bfe712627d31d68b5b822bd6da
Kosmas Pinitas <38543041+kpinitas@users.noreply.github.com>
2021-09-23 05:56:27 -0400

    Add files via upload

--- plots/display_figs.py
+++ plots/display_figs.py
@@ -7,8 +7,13 @@ Created on Mon Sep 20 03:54:27 2021
 """
 from matplotlib import pyplot as plt
 import pickle
-from matplotlib import pyplot as mp
-import numpy as np 
+import matplotlib as mpl
+
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams["figure.autolayout"] = True
+PATH='./figures/'
+EXT = '.pdf'
 
 with open(r"fig_data.pickle", "rb") as input_file:
   data = pickle.load(input_file)
@@ -17,15 +22,17 @@ with open(r"fig_data.pickle", "rb") as input_file:
 fig_counter=0
 
 def plot_fig(key,k,data, fig_counter):
+    sv=False
     if key=='classification':
         if k=='alpha':
             plt.title('Unsupervised Classification (MNIST)')
-            plt.xlabel('a_0')
+            plt.xlabel('$a_0$')
             plt.ylabel('Top-1 Accuracy')
             x=data[key][k]['alphas'][0]
             y=data[key][k]['accs'][0]
             plt.plot(x,y)
             fig_counter=fig_counter+1
+            sv=True
         elif k=='dendrites':
             plt.title('Unsupervised Classification (MNIST)')
             plt.xlabel('number of units per map')
@@ -36,7 +43,8 @@ def plot_fig(key,k,data, fig_counter):
             plt.plot(x,y1)
             plt.plot(x,y2)
             fig_counter=fig_counter+1
-            plt.legend(['DendSOM','SOM'])
+            plt.legend(['DendSOM','SOM'],frameon='False')
+            sv=True
         elif k=='rf_size':
             plt.title('Unsupervised Classification (MNIST)')
             plt.xlabel('receptive field size')
@@ -45,62 +53,72 @@ def plot_fig(key,k,data, fig_counter):
             y=data[key][k]['accs'][0]
             plt.plot(x,y)
             fig_counter=fig_counter+1
+            sv=True
     elif key=='continual':
         if k=='a_crit':
             plt.title('Class-IL (Split-Protocol)')
-            plt.xlabel('a_crit')
+            plt.xlabel('$a_{crit}$')
             plt.ylabel('Top-1 Accuracy')
             x=np.log(data[key][k]['ac'][0])
             y1=data[key][k]['cifar'][0]
             y2=data[key][k]['mnist'][0]
             plt.scatter(x,y1)
             plt.scatter(x,y2)
-            plt.legend(['CIFAR-10','MNIST'])
+            plt.legend(['CIFAR-10','MNIST'],frameon='False')
+            sv=True
             fig_counter=fig_counter+1
         elif k=='r_exp':
             plt.title('Class-IL (Split-Protocol)')
-            plt.xlabel('r_exp')
+            plt.xlabel('$r_{exp}$')
             plt.ylabel('Top-1 Accuracy')
             x=data[key][k]['rx'][0]
             y1=data[key][k]['cifar'][0]
             y2=data[key][k]['mnist'][0]
             plt.scatter(x,y1)
             plt.scatter(x,y2)
-            plt.legend(['CIFAR-10','MNIST'])
+            plt.legend(['CIFAR-10','MNIST'],frameon='False')
             fig_counter=fig_counter+1
+            sv=True
     elif key=='general_decay':
+        
+        lg=['$\lambda = '+str(int(d))+'$' for d in data[key]['lambda']]
         x=data[key]['t']
         if k=='alpha'or k=='sigma':
             y =data[key][k]
             if k=='alpha':
                 plt.title('Learning rate decay')
-                plt.ylabel('learning rate')
+                h=plt.ylabel('$a(t)$')
+                h.set_rotation(0)
             else:
                 plt.title('Neighbourhood radius decay')
-                plt.ylabel('neighbourhood radius')
+                h=plt.ylabel('$\sigma (t)$')
+                h.set_rotation(0)
             plt.xlabel('training step')
             for ii in range(len(y)):
                 plt.plot(x,y[ii])
-            plt.legend(data[key]['lambda'])
+            plt.legend(lg,frameon='False')
             fig_counter=fig_counter+1
+            sv=True
     elif key=='general_nf':
+        lg=['$t='+str(d)+'$' for d in data[key]['t']]
         x=data[key]['d']
         if k== 'h':
             y =data[key]['h']
             plt.title('Neighbourhood function decay')
-            plt.ylabel('h(t)')
+            h=plt.ylabel('$h(t)$')
+            h.set_rotation(0)
             plt.xlabel('distance from BMU')
             for ii in range(len(y)):
                 plt.plot(x,y[ii])
-            plt.legend(data[key]['t'])
+            plt.legend(lg,frameon='False')
             fig_counter=fig_counter+1
-                
-        
+            sv=True
+            
         
             
         
         
-    return fig_counter
+    return fig_counter, sv
 
 
 
@@ -108,6 +126,10 @@ def plot_fig(key,k,data, fig_counter):
 
 for key in data.keys():
     for k in data[key].keys():
-        plt.figure(fig_counter)
-        fig_counter= plot_fig(key,k,data, fig_counter)
+        fig = plt.figure(fig_counter)
+        
+        fig_counter, sv= plot_fig(key,k,data, fig_counter)
+        if sv:
+            sname=PATH+key+'_'+k+EXT
+            fig.savefig(sname)
            
